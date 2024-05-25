a66db3c96f05b4fa9bbb69d72984a53ff69362c8
Tetsewa <amatwilliams@gmail.com>
2024-05-25 16:29:04 +0200

    adding static files to be served

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-// Import `express` using `require()`
+
 const express = require('express');
 
 // Instantiate an express server instance named `app`
 // `app` is the express server that will be handling requests and responses
+
 const app = express();
+// Make the static files inside of the `public/` folder publicly accessible
+app.use(express.static('public'));
 
 // Our first route:
 app.get('/home', (request, response, next) => {
