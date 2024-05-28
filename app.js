5efcb0018837f3bdab7b8bdc7a25420a35ff2c89
Tetsewa <amatwilliams@gmail.com>
2024-05-28 14:21:40 +0200

    adding and modifying middleware

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 
 const express = require('express');
+// Require the `morgan` request logger
+const logger = require('morgan');
 
 // Instantiate an express server instance named `app`
 // `app` is the express server that will be handling requests and responses
 
 const app = express();
+
+// Setup the request logger to run on each request   // <== ADD
+app.use(logger('dev'));                       
 // Make the static files inside of the `public/` folder publicly accessible
 app.use(express.static('public'));
+
+
+app.use(myMiddleware);
+
+function myMiddleware() {
+  console.log('myMiddleware was called!');
+   // Access the request information
+   console.log('request ->', request);
+ 
+   // Modify request by adding new data to `request` object
+   request.secretValue = 'swordfish';
+   next();
+}
+
 
 // Our first route:
 app.get('/home', (request, response, next) => {
@@ -17,6 +36,10 @@ app.get('/home', (request, response, next) => {
 //our second route
 app.get('/cat', (request, response, next) => {
     response.sendFile(__dirname + '/views/cat-page.html')
+  });
+  //our third route-understanding middleware
+  app.get('/test', (request, response, next) => {
+    response.send('We made it to test!');
   });
 
 // Start the server
