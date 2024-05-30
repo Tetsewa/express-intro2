e382939ce2bd2de66465880beee1e3c959f765ed
Tetsewa <amatwilliams@gmail.com>
2024-05-30 02:19:37 +0200

    add a fourth route to send JSON response

--- app.js
+++ app.js
@@ -13,17 +13,14 @@ app.use(logger('dev'));
 // Make the static files inside of the `public/` folder publicly accessible
 app.use(express.static('public'));
 
+// JSON middleware parses (decodes) the incoming HTTP request bodies that contain JSON data and makes the data accessible on the req.body property as a JavaScript object.
+app.use(express.json()); 
 
 app.use(myMiddleware);
 
-function myMiddleware() {
+function myMiddleware(request, response, next) {
   console.log('myMiddleware was called!');
-   // Access the request information
-   console.log('request ->', request);
- 
-   // Modify request by adding new data to `request` object
-   request.secretValue = 'swordfish';
-   next();
+  next();
 }
 
 
@@ -37,10 +34,22 @@ app.get('/home', (request, response, next) => {
 app.get('/cat', (request, response, next) => {
     response.sendFile(__dirname + '/views/cat-page.html')
   });
-  //our third route-understanding middleware
-  app.get('/test', (request, response, next) => {
+//our third route-understanding middleware
+app.get('/test', (request, response, next) => {
     response.send('We made it to test!');
   });
+//our fourth route
+app.get('/data', (req, res) => {
+  const user = {
+    name: "Jane Doe",
+    age: 33,
+    profession: "Developer"
+  };
+ 
+// Send JSON data in the response
+  res.json(user);
+});
+
 
 // Start the server
 app.listen(3000, () => console.log('My first app listening on port 3000! '));
